2ed2da3e1a7a73575ed4a7d96a2b42219d3b90a9
puip34 <123201605+puip34@users.noreply.github.com>
2023-12-27 00:57:47 +0300

    Update tasks.py

--- src/resolvers/tasks.py
+++ src/resolvers/tasks.py
@@ -1,30 +1,16 @@
-from sqlalchemy.orm import Session
-from typing import List
+from server.database.db_manager import db_manager
+from server.database.models import Task
 
-from server.database.models import Task as DBTask
-from server.database.schemas import Task as TaskModel
+def create_task(task_name: str, description: str, project_id: int, creator_user_id: int, assignee_user_id: int) -> dict:
+    res = db_manager.execute(
+        query="INSERT INTO Tasks (TaskName, Description, ProjectID, CreatorUserID, AssigneeUserID) VALUES (?, ?, ?, ?, ?) RETURNING TaskID, TaskName",
+        args=(task_name, description, project_id, creator_user_id, assignee_user_id)
+    )
+    res["result"] = None if not res["result"] else Task(id=res["result"][0], name=res["result"][1])
+    return res
 
-def create_task(db: Session, task: TaskModel) -> TaskModel:
-    db_task = DBTask(**task.dict())
-    db.add(db_task)
-    db.commit()
-    db.refresh(db_task)
-    return db_task
-
-def get_task(db: Session, task_id: int) -> DBTask:
-    return db.query(DBTask).filter(DBTask.id == task_id).first()
-
-def get_all_tasks(db: Session) -> List[DBTask]:
-    return db.query(DBTask).all()
-
-def update_task(db: Session, task_id: int, updated_task: TaskModel) -> DBTask:
-    db_task = get_task(db=db, task_id=task_id)
-    for key, value in updated_task.dict(exclude_unset=True).items():
-        setattr(db_task, key, value)
-    db.commit()
-    db.refresh(db_task)
-    return db_task
-
-def delete_task(db: Session, task_id: int) -> None:
-    db.query(DBTask).filter(DBTask.id == task_id).delete()
-    db.commit()
+def get_all_tasks() -> dict:
+    res = db_manager.execute(query="SELECT * FROM Tasks", many=True)
+    list_tasks = [Task(id=item[0], name=item[1]) for item in res["result"]] if res["result"] else []
+    res["result"] = None if not list_tasks else list_tasks
+    return res
