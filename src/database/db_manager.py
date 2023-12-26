52f34377ecd588eedb77ad13eeee6e097be7ada3
puip34 <123201605+puip34@users.noreply.github.com>
2023-12-27 01:24:43 +0300

    Update db_manager.py

--- src/database/db_manager.py
+++ src/database/db_manager.py
@@ -1,7 +1,27 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import sqlite3
+from typing import List, Tuple
 
-from server.config import DATABASE_URL
+class DBManager:
+    def __init__(self, db_path: str = "task_manager.db"):
+        self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor()
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    def execute(self, query: str, args: Tuple = None, many: bool = False) -> dict:
+        try:
+            if args:
+                self.cursor.execute(query, args)
+            else:
+                self.cursor.execute(query)
+
+            if many:
+                result = self.cursor.fetchall()
+            else:
+                result = self.cursor.fetchone()
+
+            self.conn.commit()
+            return {"result": result}
+        except Exception as e:
+            return {"error": str(e)}
+
+# Singleton pattern to ensure a single instance of DBManager
+db_manager = DBManager()
